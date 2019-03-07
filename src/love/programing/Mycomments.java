bd4bc5e76892805f13c06fd5be02294e0531e001
Umadevi23 <uma_gorakati@yahoo.co.in>
2019-03-07 11:37:21 +0000

    JavaCommentsProject

---
+++ src/love/programing/Mycomments.java
@@ -0,0 +1,26 @@
+package love.programing;
+
+import java.util.*;
+//This is my first my java comment programe
+public class Mycomments {
+
+
+    //this is my first public comment
+    public static void main(String[] args) {
+
+
+        //this is my main comment
+        System.out.println("My First Project comments");
+        int =8
+                char c= 'a'
+        '
+                //declaration
+        1=8
+                //intialisation
+        c= "a"
+        //this is my println comment
+        /* this is first comment
+        this comment for paragraph
+         */
+    }
+}
